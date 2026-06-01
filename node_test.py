975174a8aa6608c2de317686aefef0c78f7d7078
Niranjan Gadgil <niranjanvgce123@gst.sies.edu.in>
2026-06-01 17:10:03 +0530

    chore: untrack and ignore test files

--- node_test.py
+++
@@ -1,5 +0,0 @@
-from sandbox_node import Node as nd
-
-node = nd()
-result = node.not_starting_server("""python -c "import os; print(os.getcwd())" """)
-print(result)
--- tools_test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from tools import append_file_to_wkng_dir
-
-print(os.getcwd())
-append_file_to_wkng_dir("file.txt", "Hello World")
